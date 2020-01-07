2dbefbe93ccd6b51f6dba7790c5fc196b791ad31
Rajolep <priyarajole777@gmail.com>
2020-01-07 09:26:32 +0530

    firstcommit

--- codes/quadr/quadexer.py
+++ codes/quadr/quadexer.py
@@ -1,90 +1,98 @@
-#Code by PRIYANKA R
-#December 31, 2019
+#Code by GVV Sharma
+#December 7, 2019
 #released under GNU GPL
+#Drawing a parallelogram  given 2 sides and a diagonal
 import numpy as np
 import matplotlib.pyplot as plt
-import math
 from coeffs import *
 
-a = 3.5
-b = 6.5
 
-angD=(np.pi/180)*105
-c = np.sqrt(a**2+b**2-2*a*b*np.cos(angD))
+
+
+
+a = 3
+b = 5
+angP = np.pi/180*105
+c = np.sqrt(a**2+b**2-2*a*b*np.cos(angP))
+
+print(c)
+
+#Coordinates of P
 p = (a**2 + c**2-b**2 )/(2*a)
 q = np.sqrt(c**2-p**2)
-ang=math.radians(60)
-Q=np.array([0,0])
-R=np.array([a,0])
-P=np.array([p,q])
+print(p,q)
+#Coordinates of C
+f = 8
+ang = np.pi/180*75
+g = np.sqrt(f**2+c**2-2*f*c*np.cos(ang))
+print(g)
+p1 = (c**2 + f**2-g**2 )/(2*c)
+q1 = np.sqrt(f**2-p1**2)
+print(p1,q1)
+#Parallelogram vertices
+P = np.array([p,q]) 
+Q = np.array([0,0]) 
+R = np.array([a,0]) 
+C = np.array([p1,q1]) 
+#Mid point of BD
+O =(Q+P)/2
+#Finding B
+B = 2*O-R
+A=np.array([-(5-B[0]),(B[1])])
+h = 5
+i=3
+angB = np.pi/180*105
+j = np.sqrt(h**2+i**2-2*h*i*np.cos(angB))
+print(j)
+O1 = (A+C)/2
+D = 2*O1-B
 
 print(P)
-A=np.array([-5.16,6.2375])
-B=np.array([1.67905,6.27879])
-D=np.array([-4.092196,9.0388])
-an=np.tan((np.pi/180)*135)
-#direction vector of PB
-m1=np.array([1,an])
-#normal vector of PB
-n1 = np.matmul(omat,m1)
-#Direction vector of PB = QB
-m2=P-R
-#normal vector of QB
-n2 = np.matmul(omat,m2)
-#print(n2)
-#T = line_intersect(n1,A,n2,D)
-N=np.vstack((n1,n2))
-#print(n1,n2,N)
-r = np.zeros(2)
-r[0] = n1@P
-r[1] = n2@Q
-#print(r)
-#Intersection
-C=np.linalg.inv(N)@r
-d=np.sqrt((C[0])**2+(C[1])**2)
-angz=(np.pi/180)*75
-e = np.sqrt(a**2+d**2-2*a*d*np.cos(angz))
-p = (a**2 + e**2-d**2 )/(2*a)
-q = np.sqrt(e**2-p**2)
-print(e)
-
+print(B)
 print(C)
+print(D)
+print(A)
+
 #Generating all lines
+
 x_QR = line_gen(Q,R)
-x_RP = line_gen(R,P)
-x_QP = line_gen(Q,P)
-x_PC = line_gen(P,C)
+x_PQ = line_gen(P,Q)
+x_BP = line_gen(B,P)
+x_PR = line_gen(P,R)
 x_QC = line_gen(Q,C)
-x_AP = line_gen(A,P)
-x_AQ = line_gen(A,Q)
-x_AD = line_gen(A,D)
+x_CP = line_gen(C,P)
+x_BA = line_gen(B,A)
+x_QA = line_gen(Q,A)
 x_AC = line_gen(A,C)
+x_AD = line_gen(A,D)
 x_DC = line_gen(D,C)
 #Plotting all lines
+
 plt.plot(x_QR[0,:],x_QR[1,:],label='$QR$')
-plt.plot(x_RP[0,:],x_RP[1,:],label='$RP$')
-plt.plot(x_QP[0,:],x_QP[1,:],label='$QP$')
-plt.plot(x_PC[0,:],x_PC[1,:],label='$PC$')
+plt.plot(x_PQ[0,:],x_PQ[1,:],label='$PQ$')
+plt.plot(x_BP[0,:],x_BP[1,:],label='$BP$')
+plt.plot(x_PR[0,:],x_PR[1,:],label='$PR$')
 plt.plot(x_QC[0,:],x_QC[1,:],label='$QC$')
-plt.plot(x_DC[0,:],x_DC[1,:],label='$DC$')
+plt.plot(x_CP[0,:],x_CP[1,:],label='$CP$')
+plt.plot(x_BA[0,:],x_BA[1,:],label='$BA$')
+plt.plot(x_QA[0,:],x_QA[1,:],label='$QA$')
 plt.plot(x_AC[0,:],x_AC[1,:],label='$AC$')
-plt.plot(x_AQ[0,:],x_AQ[1,:],label='$AQ$')
-plt.plot(x_AP[0,:],x_AP[1,:],label='$AP$')
 plt.plot(x_AD[0,:],x_AD[1,:],label='$AD$')
-plt.plot(Q[0], Q[1], 'o')
-plt.text(Q[0] * (1 + 0.1), Q[1] * (1 - 0.1) , 'Q')
-plt.plot(R[0], R[1], 'o')
-plt.text(R[0] * (1 +0.1), R[1] * (1) , 'R')
-plt.plot(P[0], P[1], 'o')
-plt.text(P[0] * (1 + 0.03), P[1] * (1 - 0.1) , 'P')
-plt.plot(C[0], C[1], 'o')
-plt.text(C[0] * (1 + 0.03), C[1] * (1 - 0.1) , 'C')
+plt.plot(x_DC[0,:],x_DC[1,:],label='$DC$')
 plt.plot(A[0], A[1], 'o')
-plt.text(A[0] * (1 + 0.03), A[1] * (1 - 0.1) , 'A')
-plt.plot(D[0], D[1], 'o')
-plt.text(D[0] * (1 + 0.03), D[1] * (1 - 0.1) , 'D')
+plt.text(A[0] * (1 + 0.1), A[1] * (1 - 0.1) , 'A(-3.7,4.82)')
 plt.plot(B[0], B[1], 'o')
-plt.text(B[0] * (1 + 0.03), B[1] * (1 - 0.1) , 'B')
+plt.text(B[0] * (1 + 0.1), B[1] * (1 - 0.1) , 'B(1.29,4.82)')
+plt.plot(Q[0], Q[1], 'o')
+plt.text(Q[0] * (1 - 0.2), Q[1] * (1) , 'Q(0,0)')
+plt.plot(R[0], R[1], 'o')
+plt.text(R[0] * (1 + 0.03), R[1] * (1 - 0.1) , 'R(3,0)')
+plt.plot(P[0], P[1], 'o')
+plt.text(P[0] * (1 + 0.03), P[1] * (1 - 0.1) , 'P(4.29,4.82)')
+plt.plot(C[0], C[1], 'o')
+plt.text(C[0] * (1 + 0.03), C[1] * (1 - 0.1) , 'C(2.07,7.72)')
+plt.plot(D[0], D[1], 'o')
+plt.text(D[0] * (1 + 0.03), D[1] * (1 - 0.1) , 'D(-2.92,7.72)')
 plt.xlabel('$x$')
 plt.ylabel('$y$')
 plt.legend(loc='best')
@@ -97,6 +105,8 @@ plt.axis('equal')
 #subprocess.run(shlex.split("termux-open ./figs/quad/pgm_sss.pdf"))
 #else
 plt.show()
+
+
 
 
 
